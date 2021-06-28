139118ff679b9c59028ef495aa7a6feebbdafeba
Rafael Viana <rafaelviana544@yahoo.com>
2021-06-28 18:18:04 -0300

    Hello Springboot

---
+++ Springboot01/src/main/java/br/com/rafael/Startup.java
@@ -0,0 +1,14 @@
+package br.com.rafael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Startup {
+	
+public static void main(String[]args) {
+	SpringApplication.run(Startup.class,args);
+	
+   }
+}
